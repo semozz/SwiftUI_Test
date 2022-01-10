8d73c287249cdf3e865d62a00407c3f480b1d765
SangWoo <semo75@gmail.com>
2022-01-10 10:37:52 +0900

    Initial Commit

---
+++ SwiftUIUberEatsMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIUberEatsMenu
+//
+//  Created by SangWoo on 2022/01/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIUberEatsMenu/SwiftUIUberEatsMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIUberEatsMenuApp.swift
+//  SwiftUIUberEatsMenu
+//
+//  Created by SangWoo on 2022/01/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIUberEatsMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
